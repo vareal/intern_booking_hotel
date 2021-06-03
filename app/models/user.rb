bcd7c28a8097b8bd2a9d8718c4d3ee398718a7a8
tvbach <tvbach20@gmail.com>
2021-06-03 23:05:32 +0700

    task login

---
+++ app/models/user.rb
@@ -0,0 +1,8 @@
+class User < ApplicationRecord
+  # Include default devise modules. Others available are:
+  # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  devise :database_authenticatable, :registerable,
+         :recoverable, :rememberable, :validatable
+
+  enum role: {user: 0, publisher:1, admin: 2}
+end
--- config/application.rb
+++ config/application.rb
@@ -1,6 +1,6 @@
-require_relative 'boot'
+require_relative "boot"
 
-require 'rails/all'
+require "rails/all"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
@@ -10,10 +10,17 @@ module InternBookingHotel
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 6.0
-
+    config.i18n.load_path += Dir[Rails.root.join("config", "locales", "**",
+      "*.{rb,yml}")]
+    config.i18n.available_locales = [:en, :vi]
+    config.i18n.default_locale = :en
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
     # the framework and any gems in your application.
+    config.to_prepare do
+      Devise::SessionsController.layout "layouts/devise/application"
+      Devise::RegistrationsController.layout "layouts/devise/application"
+    end
   end
 end
