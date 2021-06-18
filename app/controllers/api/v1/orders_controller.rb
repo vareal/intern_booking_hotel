1438759c25efe032a7ae7b91a5024795ee836978
tvbach <tvbach20@gmail.com>
2021-06-18 11:21:34 +0700

    fix bug i18n for booking room

--- app/controllers/api/v1/orders_controller.rb
+++ app/controllers/api/v1/orders_controller.rb
@@ -2,12 +2,12 @@ class Api::V1::OrdersController < Api::V1::ApiController
   def create
     order = current_user.orders.build order_params
     if order.save
-      msg = { status: t "message.order.status_ok",
-        message:  t "message.order.sucssess" }
+      msg = { status: t("message.order.status_ok"),
+        message:  t("message.order.sucssess") }
       json_response order
     else
-      msg = { status: t "message.order.status_not",
-        message:  t "message.order.danger" }
+      msg = { status: t("message.order.status_not"),
+        message:  t("message.order.danger") }
     end
     json_response msg
   end
